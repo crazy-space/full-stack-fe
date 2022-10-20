1ad5b24bdf923deec57cd59be2055e171cc56c1c
Youzege <youzegehq@gmail.com>
2022-10-20 15:37:56 +0800

    feat: 初始化FullStack

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: Youzege
+ * @Date: 2022-10-20 15:31:56
+ * @LastEditors: Youzege
+ * @LastEditTime: 2022-10-20 15:33:25
+ */
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <p>Hello FullStack</p>
+    </div>
+  )
+}
+
+export default App
